adaec2b3d901897859457c350473ffcb24e668f9
dpGGagainis <39499184+gloopjuice@users.noreply.github.com>
2025-06-02 01:58:52 +0300

    qwe
    qwe

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,15 @@ export default defineConfig({
       sourcemap: true
     },
     server: {
-      port: 5173,  
+      port: 5173,
+      host: '0.0.0.0',
+      hmr: {
+          host: '64.227.105.115',
+          port: 5173
+      },
+      watch: {
+          usePolling: true
+      }
     },
   plugins: [
     vue(),
